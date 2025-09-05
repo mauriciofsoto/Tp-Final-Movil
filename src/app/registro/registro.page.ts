e01271efeb34764b4c8ee63dbd532f6936deb071
luciano <luciano.delapena@gmail.com>
2025-09-04 23:01:21 -0300

    cree registro, en una carpeta registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,42 +1,29 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular'; // necesario para ion-header, ion-input, etc.
-import { CommonModule } from '@angular/common'; // necesario para *ngIf, *ngFor
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.page.html',
-  styleUrls: ['./registro.page.scss'],
-  standalone: true,
-  imports: [IonicModule, CommonModule] // FormsModule no hace falta si no usás ngModel
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+  standalone: false,
 })
-export class RegistroPage {
+export class Tab1Page {
 
   constructor(private router: Router) {}
 
-  registrarse(
-    nombre: string, 
-    nombreUsuario: string, 
-    email: string, 
-    nacimiento: string, 
-    password: string, 
-    repPassword: string
-  ) {
-    if (!nombre || !nombreUsuario || !email || !nacimiento || !password || !repPassword) {
-      alert('Completa todos los campos');
-      return;
-    }
+  iniciarSesion(usuario: string, password: string, Reppassword: string, fecha: Date) {
+  console.log('Usuario:', usuario);
+  console.log('Password:', password);
 
-    if (password !== repPassword) {
-      alert('Las contraseñas no coinciden');
-      return;
-    }
-
-    alert('Registro exitoso');
-    this.router.navigate(['/tabs/tab1']);
+  // Aquí podés validar y navegar
+  if (!usuario || !password || !Reppassword || !fecha) {
+    alert('Completa todos los campos');
+    return;
   }
 
-  volverLogin() {
-    this.router.navigate(['/tabs/tab1']);
+  this.router.navigate(['/tabs/tab2']); // redirige a Inicio
+}
+  irRegistro() {
+    this.router.navigate(['/tabs/tab1']); // Página de registro fuera de tabs
   }
 }
